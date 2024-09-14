510a3c3e9b985ba08ad6839093fd15045bc3b691
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-09-14 21:36:18 +0530

    updated

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import SearchOrder from "../features/order/SearchOrder";
 const Header = () => {
   return (
     <header className="bg-yellow-400 flex justify-between items-center p-4">
-      <Link to="/"><span className="uppercase">Fast React Pizza Co.</span></Link>
+      <Link to="/"><span className="uppercase tracking-[2px]">Fast React Pizza Co.</span></Link>
 
       <SearchOrder/>
 
