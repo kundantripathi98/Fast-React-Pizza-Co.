8d8fb62449a80ccdf293e8396e03fc671dc7562e
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-09-14 21:47:31 +0530

    updated

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,21 +1,27 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const SearchOrder = () => {
-    const [query, setQuery] = useState('');
-    const navigate = useNavigate();
+function SearchOrder() {
+  const [query, setQuery] = useState('');
+  const navigate = useNavigate();
 
-    function handleSubmit(e){
-        e.preventDefault();
-        navigate(`/order/${query}`);
-        setQuery('');
-    }
-    
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery('');
+  }
+
   return (
     <form onSubmit={handleSubmit}>
-        <input className="rounded-2xl p-2 pr-5" placeholder="Search Order #" value={query} onChange={(e)=>setQuery(e.target.value)} type="text"/>
+      <input
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
+      />
     </form>
   );
-};
+}
 
 export default SearchOrder;
