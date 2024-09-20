fd5bdbf5d36ea33e5cacc6d76b7b2d62a08e8623
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-09-20 19:08:36 +0530

    updated

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -18,7 +18,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small" onClick={handleDelete}>Delete</Button>
+        {/* <Button type="small" onClick={handleDelete}>Delete</Button> */}
       </div>
     </li>
   );
---
+++ src/features/cart/DeleteItem.jsx
@@ -0,0 +1,9 @@
+const DeleteItem = () => {
+  return (
+    <div>
+       <Button type="small" onClick={handleDelete}>Delete</Button>
+    </div>
+  );
+};
+
+export default DeleteItem;
